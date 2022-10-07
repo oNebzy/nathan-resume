4f250c47d58bc794a9b633d8d5f38e127dfd61ab
oNebzy <neb0010@mix.wvu.edu>
2022-10-07 12:23:06 -0400

    skills comp update

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,12 +4,18 @@ const Skills = () => {
   return (
     <div name="Skills" className='w-full h-screen bg-[#d3ece2]'>
       <div className='max-w-[1000px] mx-auto px-8 flex flex-col h-full'>
-        <div className=''>
-          <p className='text-[#7b857f] italic font-semibold inline border-b-4 border-[#358f60] mt-[20px] text-2xl'>{'Skills & Experience'}</p>
+        <div className='mt-[20px]'>
+          <p className="font-Kaushanscript text-[#7b857f]">{'<h1>'}</p>
+          <p className='text-[#358f60] italic font-bold md:text-6xl text-4xl'>Skills {'&'} Experience</p>
+          <p className="font-Kaushanscript text-[#7b857f]">{'<h1/>'}</p>
         </div>
         <div>
-          <p className='text-[#637269] mt-3'>{'//'} The scope of my experience has given me the opportunity to work with multiple technologies. Here are the ones I've used recently:</p>
+          <p className='text-[#637269] mt-3'>
+            The scope of my work and experience has given me the opportunity to work with multiple technologies and industries.
+            From retail to agriculture, medical to education, I've been able to produce and deliver quality solutions and applications.
+          </p>
         </div>
+        
       </div>
     </div>
   )
