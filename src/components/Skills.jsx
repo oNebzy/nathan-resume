77080137a1ecc805c15754ea66f49198ceac8350
oNebzy <neb0010@mix.wvu.edu>
2022-10-07 14:16:02 -0400

    Added skills tiles to skills component

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,24 +1,268 @@
-import React from 'react'
+import React from "react";
+
+
+//import redux from "../assets/logos/redux.png";
+//import eclipse from"../assets/logos/eclipse.png";
+//import paper1 from "../assets/logos/paper1.png";
+import css from "../assets/logos/css.png";
+import Dart from "../assets/logos/Dart.png";
+import eas from "../assets/logos/eas.png";
+import expo from "../assets/logos/expo.png";
+import firebase from "../assets/logos/firebase.png";
+import git from "../assets/logos/git.png";
+import html from "../assets/logos/html.png";
+import java from "../assets/logos/java.png";
+import javascript from "../assets/logos/javascript.png";
+import jira from "../assets/logos/jira.png";
+import mongo from "../assets/logos/mongo.png";
+import MUI from "../assets/logos/MUI.png";
+import netlify from "../assets/logos/netlify.png";
+import npm from "../assets/logos/npm.png";
+import react from "../assets/logos/react.png";
+import Sass from "../assets/logos/Sass.png";
+import stripe from "../assets/logos/stripe.png";
+import tailwind from "../assets/logos/tailwind.png";
+import Typescript from "../assets/logos/Typescript.png";
+import VScode from "../assets/logos/VScode.png";
+import workato from "../assets/logos/workato.png";
+import xcode from "../assets/logos/xcode.png";
+import yarn from "../assets/logos/yarn.png";
+import zapier from "../assets/logos/zapier.png";
+
 
 const Skills = () => {
   return (
-    <div name="Skills" className='w-full h-screen bg-[#d3ece2]'>
-      <div className='max-w-[1000px] mx-auto px-8 flex flex-col h-full'>
-        <div className='mt-[20px]'>
-          <p className="font-Kaushanscript text-[#7b857f]">{'<h1>'}</p>
-          <p className='text-[#358f60] italic font-bold md:text-6xl text-4xl'>Skills {'&'} Experience</p>
-          <p className="font-Kaushanscript text-[#7b857f]">{'<h1/>'}</p>
-        </div>
-        <div>
-          <p className='text-[#637269] mt-3'>
-            The scope of my work and experience has given me the opportunity to work with multiple technologies and industries.
-            From retail to agriculture, medical to education, I've been able to produce and deliver quality solutions and applications.
+    <div name="Skills" className="w-full h-screen bg-[#d3ece2]">
+      <div className="max-w-[1000px] mx-auto px-8 flex flex-col h-full">
+        {/* header container */}
+        <div className="mt-[20px]">
+          <p className="font-Kaushanscript text-[#7b857f]">{"<h1>"}</p>
+          <p className="text-[#358f60] italic font-bold md:text-6xl text-4xl">
+            Skills {"&"} Experience
           </p>
+          <p className="font-Kaushanscript text-[#7b857f]">{"<h1/>"}</p>
         </div>
-        
+
+        {/* text container */}
+        <div>
+          <p className="text-[#637269] mt-3">
+            The scope of my work and experience has given me the opportunity to
+            work with multiple technologies and industries. From retail to
+            agriculture, medical to education, I've been able to produce and
+            deliver quality solutions and applications.
+          </p>
+          <p className="mt-6 text-[#637269]">
+            {"//"} These are the technologies I've worked with:
+          </p>
+          <p>need new image for eclipse redux paper</p>
+        </div>
+
+        {/* tech tiles container */}
+        <div className="w-full grid grid-cols-5 sm:grid-cols-8 gap-4 text-center text-white font-medium">
+
+          <div className="bg-[#414d46] rounded-lg shadow-lg w-full h-full flex flex-col p-2 justify-evenly  hover:animate-jiggle">
+            <div className="w-full h-full flex justify-center items-center">
+              <img src={css} className="max-h-10"></img>
+            </div>
+            <div>CSS</div>
+          </div>
+
+          <div className="bg-[#414d46] rounded-lg shadow-lg w-full h-full flex flex-col p-2 justify-evenly hover:animate-jiggle">
+            <div className="w-full h-full flex justify-center items-center">
+              <img src={Dart} className="max-h-10"></img>
+            </div>
+            <div>Dart</div>
+          </div>
+
+          <div className="bg-[#414d46] rounded-lg shadow-lg w-full h-full flex flex-col p-2 justify-evenly hover:animate-jiggle">
+            <div className="w-full h-full flex justify-center items-center">
+              <img src={eas} className="max-h-10"></img>
+            </div>
+            <div>EAS</div>
+          </div>
+
+          {/* needs new image */}
+          {/* <div className="bg-[#414d46] rounded-lg shadow-lg w-full h-full flex flex-col p-2 justify-evenly hover:animate-jiggle">
+            <div className="w-full h-full flex justify-center items-center">
+              <img src={eclipse} className="max-h-10"></img>
+            </div>
+            <div>Eclipse</div>
+          </div> */}
+
+          <div className="bg-[#414d46] rounded-lg shadow-lg w-full h-full flex flex-col p-2 justify-evenly hover:animate-jiggle">
+            <div className="w-full h-full flex justify-center items-center">
+              <img src={expo} className="max-h-10"></img>
+            </div>
+            <div>expo</div>
+          </div>
+
+          <div className="bg-[#414d46] rounded-lg shadow-lg w-full h-full flex flex-col p-2 justify-evenly hover:animate-jiggle">
+            <div className="w-full h-full flex justify-center items-center">
+              <img src={firebase} className="max-h-10"></img>
+            </div>
+            <div>firebase</div>
+          </div>
+
+          <div className="bg-[#414d46] rounded-lg shadow-lg w-full h-full flex flex-col p-2 justify-evenly hover:animate-jiggle">
+            <div className="w-full h-full flex justify-center items-center">
+              <img src={git} className="max-h-10"></img>
+            </div>
+            <div>git</div>
+          </div>
+
+          <div className="bg-[#414d46] rounded-lg shadow-lg w-full h-full flex flex-col p-2 justify-evenly hover:animate-jiggle">
+            <div className="w-full h-full flex justify-center items-center">
+              <img src={html} className="max-h-10"></img>
+            </div>
+            <div>html</div>
+          </div>
+
+          {/* need new image */}
+          {/* <div className="bg-[#414d46] rounded-lg shadow-lg w-full h-full flex flex-col p-2 justify-evenly hover:animate-jiggle">
+            <div className="w-full h-full flex justify-center items-center">
+              <img src={paper1} className="max-h-10"></img>
+            </div>
+            <div>paper</div>
+          </div> */}
+
+          <div className="bg-[#414d46] rounded-lg shadow-lg w-full h-full flex flex-col p-2 justify-evenly hover:animate-jiggle">
+            <div className="w-full h-full flex justify-center items-center">
+              <img src={java} className="max-h-10"></img>
+            </div>
+            <div>java</div>
+          </div>
+
+          <div className="bg-[#414d46] rounded-lg shadow-lg w-full h-full flex flex-col p-2 justify-evenly hover:animate-jiggle">
+            <div className="w-full h-full flex justify-center items-center">
+              <img src={javascript} className="max-h-10"></img>
+            </div>
+            <div>javascript</div>
+          </div>
+
+          <div className="bg-[#414d46] rounded-lg shadow-lg w-full h-full flex flex-col p-2 justify-evenly hover:animate-jiggle">
+            <div className="w-full h-full flex justify-center items-center">
+              <img src={jira} className="max-h-10"></img>
+            </div>
+            <div>jira</div>
+          </div>
+
+          <div className="bg-[#414d46] rounded-lg shadow-lg w-full h-full flex flex-col p-2 justify-evenly hover:animate-jiggle">
+            <div className="w-full h-full flex justify-center items-center">
+              <img src={mongo} className="max-h-10"></img>
+            </div>
+            <div>mongo</div>
+          </div>
+
+          <div className="bg-[#414d46] rounded-lg shadow-lg w-full h-full flex flex-col p-2 justify-evenly hover:animate-jiggle">
+            <div className="w-full h-full flex justify-center items-center">
+              <img src={MUI} className="max-h-10"></img>
+            </div>
+            <div>MUI</div>
+          </div>
+
+          <div className="bg-[#414d46] rounded-lg shadow-lg w-full h-full flex flex-col p-2 justify-evenly hover:animate-jiggle">
+            <div className="w-full h-full flex justify-center items-center">
+              <img src={netlify} className="max-h-10"></img>
+            </div>
+            <div>netlify</div>
+          </div>
+
+          <div className="bg-[#414d46] rounded-lg shadow-lg w-full h-full flex flex-col p-2 justify-evenly hover:animate-jiggle">
+            <div className="w-full h-full flex justify-center items-center">
+              <img src={npm} className="max-h-10"></img>
+            </div>
+            <div>npm</div>
+          </div>
+
+          <div className="bg-[#414d46] rounded-lg shadow-lg w-full h-full flex flex-col p-2 justify-evenly hover:animate-jiggle">
+            <div className="w-full h-full flex justify-center items-center">
+              <img src={react} className="max-h-10"></img>
+            </div>
+            <div>react</div>
+          </div>
+
+          {/* need new image */}
+          {/* <div className="bg-[#414d46] rounded-lg shadow-lg w-full h-full flex flex-col p-2 justify-evenly hover:animate-jiggle">
+            <div className="w-full h-full flex justify-center items-center">
+              <img src={redux} className="max-h-10"></img>
+            </div>
+            <div>redux</div>
+          </div> */}
+
+          <div className="bg-[#414d46] rounded-lg shadow-lg w-full h-full flex flex-col p-2 justify-evenly hover:animate-jiggle">
+            <div className="w-full h-full flex justify-center items-center">
+              <img src={Sass} className="max-h-10"></img>
+            </div>
+            <div>Sass</div>
+          </div>
+
+          <div className="bg-[#414d46] rounded-lg shadow-lg w-full h-full flex flex-col p-2 justify-evenly hover:animate-jiggle">
+            <div className="w-full h-full flex justify-center items-center">
+              <img src={stripe} className="max-h-10"></img>
+            </div>
+            <div>stripe</div>
+          </div>
+
+          <div className="bg-[#414d46] rounded-lg shadow-lg w-full h-full flex flex-col p-2 justify-evenly hover:animate-jiggle">
+            <div className="w-full h-full flex justify-center items-center">
+              <img src={tailwind} className="max-h-10"></img>
+            </div>
+            <div>tailwind</div>
+          </div>
+
+          <div className="bg-[#414d46] rounded-lg shadow-lg w-full h-full flex flex-col p-2 justify-evenly hover:animate-jiggle">
+            <div className="w-full h-full flex justify-center items-center">
+              <img src={Typescript} className="max-h-10"></img>
+            </div>
+            <div>Typescript</div>
+          </div>
+
+          <div className="bg-[#414d46] rounded-lg shadow-lg w-full h-full flex flex-col p-2 justify-evenly hover:animate-jiggle">
+            <div className="w-full h-full flex justify-center items-center">
+              <img src={VScode} className="max-h-10"></img>
+            </div>
+            <div>VScode</div>
+          </div>
+
+          <div className="bg-[#414d46] rounded-lg shadow-lg w-full h-full flex flex-col p-2 justify-evenly hover:animate-jiggle">
+            <div className="w-full h-full flex justify-center items-center">
+              <img src={workato} className="max-h-10"></img>
+            </div>
+            <div>workato</div>
+          </div>
+
+          <div className="bg-[#414d46] rounded-lg shadow-lg w-full h-full flex flex-col p-2 justify-evenly hover:animate-jiggle">
+            <div className="w-full h-full flex justify-center items-center">
+              <img src={xcode} className="max-h-10"></img>
+            </div>
+            <div>xcode</div>
+          </div>
+
+          <div className="bg-[#414d46] rounded-lg shadow-lg w-full h-full flex flex-col p-2 justify-evenly hover:animate-jiggle">
+            <div className="w-full h-full flex justify-center items-center">
+              <img src={yarn} className="max-h-10"></img>
+            </div>
+            <div>yarn</div>
+          </div>
+
+          <div className="bg-[#414d46] rounded-lg shadow-lg w-full h-full flex flex-col p-2 justify-evenly hover:animate-jiggle">
+            <div className="w-full h-full flex justify-center items-center">
+              <img src={zapier} className="max-h-10"></img>
+            </div>
+            <div>zapier</div>
+          </div>
+
+
+          
+
+          
+          
+
+
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Skills
+export default Skills;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,21 @@ module.exports = {
     extend: {
       fontFamily: {
         Kaushanscript: ['Kaushan Script']
+      },
+      animation: {
+        wiggle: 'wiggle .5s ease-in-out infinite',
+        jiggle: 'jiggle .6s infinite'
+      },
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        },
+        jiggle: {
+          '0%, 100%': { transform: 'translateY(-15%) rotate(-15deg)'},
+          '50%': { transform: 'translateY(5%) rotate(-15deg)' },
+          '25%, 75%': {transform: 'rotate(10deg)'}
+        }
       }
     },
   },
