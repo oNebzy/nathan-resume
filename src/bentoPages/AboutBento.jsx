5f3479ef4105e11881876e38723d5d692f1e463a
nathan-burd <nathanburd.dev@gmail.com>
2024-03-01 16:45:09 -0500

    finished about bento mobile responsiveness update

--- src/bentoPages/AboutBento.jsx
+++ src/bentoPages/AboutBento.jsx
@@ -21,7 +21,7 @@ function AboutBento() {
   return (
     <div className="w-full h-full md:mt-7" id="about">
       {/* Top Row */}
-      <div className='w-[100%] h-[14%] mb-6 grid grid-cols-10 gap-6 md:px-0 px-4'>
+      <div className='w-[100%] h-[14%] md:mb-6 mb-3 grid grid-cols-10 md:gap-6 gap-3 md:px-0 px-4'>
         <motion.div
           className='col-span-7'
           initial={{opacity: 0, y: "-50%"}}
@@ -140,19 +140,23 @@ function AboutBento() {
       {/* END Web View second and third rows */}
 
       {/* Mobile view 2nd+ Rows*/}
-      <div className='md:hidden grid w-[100%] h-[30%] mb-6 grid-cols-11 gap-6 md:px-0 px-4'>
+
+      {/* Mobile 2nd row container*/}
+      <div className='md:hidden grid w-[100%] h-[30%] md:mb-6 mb-3 grid-cols-11 gap-4 md:px-0 px-4'>
+        
+        {/* Second Row Left Column Container */}
         {/* skills Box*/}
         <motion.div
-          className='col-span-7'
+          className='col-span-6'
           initial={{opacity: 0, x: "50%"}}
           animate={{opacity: 1, x: 0, transition:{duration: 2, delay: 1}}}
           exit={{opacity: 0, x: "30%", transition:{duration: 1}}}
         >
-          <div className='h-[100%] col-span-7 bg-neutral-800 shadow-md shadow-[#7f8285] rounded-xl border-2 border-[#ffffff] transition hover:shadow-lg hover:shadow-pink-300 ease-in-out duration-300'> 
+          <div className='h-fit col-span-7 bg-neutral-800 shadow-md shadow-[#7f8285] rounded-xl border-2 border-[#ffffff] transition hover:shadow-lg hover:shadow-pink-300 ease-in-out duration-300'> 
             <div className='m-3 text-lg text-white '>
               {'</>'} Skills
             </div>
-            <div className="w-full grid grid-cols-2 text-center text-white font-medium text-xs">
+            <div className="w-full grid grid-cols-2 text-center text-white font-medium text-xs pb-2">
               <SkillBox imageImport={javascript} skill="Javascript"/>
               <SkillBox imageImport={html} skill="HTML"/>
               <SkillBox imageImport={css} skill="CSS"/>
@@ -168,34 +172,67 @@ function AboutBento() {
           </div>
         </motion.div>
         
-        {/*Project Count Box */}
-        <motion.div
-          className='col-span-4'
-          initial={{opacity: 0, x: "50%"}}
-          animate={{opacity: 1, x: 0, transition:{duration: 2, delay: 1.7}}}
-          exit={{opacity: 0, x: "50%", transition:{duration: .75}}}
-        >
-          <div className='h-[100%] col-span-4 justify-center align-middle items-center flex flex-col pb-4 bg-neutral-800 shadow-md shadow-[#7f8285] rounded-xl border-2 border-[#ffffff] transition hover:shadow-lg hover:shadow-pink-300 ease-in-out duration-300 text-white'>
-            <div className='text-[100px] animate-text bg-gradient-to-r from-emerald-500 to-pink-400 bg-clip-text text-transparent font-semibold'>11+</div>
-            <div className='text-xl '>Profesional Projects</div>
-          </div>
-        </motion.div>
-        {/* Project Industries Box*/}
-        <motion.div
-          className='col-span-3 flex flex-col'
-          initial={{opacity: 0, y: "50%"}}
-          animate={{opacity: 1, y: 0, transition:{duration: 2, delay: 1}}}
-          exit={{opacity: 0, y: "50%", transition:{duration: 1}}}
-        >
-          <div className='col-span-3 py-3 mb-4 flex flex-col items-center justify-center bg-neutral-800 shadow-md shadow-[#7f8285] rounded-xl border-2 border-[#ffffff] transition hover:shadow-lg hover:shadow-pink-300 ease-in-out duration-300'>
-            <div className='text-base text-white'>Projects in</div>
-            <div className='text-[72px] bg-gradient-to-br from-pink-500 to-pink-300 bg-clip-text text-transparent font-semibold mt-3 mb-3'>8</div>
-            <div className='text-base text-white'>unique industries</div>
-          </div>
-          <ResumeButton/>
-        </motion.div>
-        
+        {/* Second Row Right Column Container */}
+        <div className='col-span-5'>
+          <motion.div
+            className='col-span-5'
+            initial={{opacity: 0, x: "50%"}}
+            animate={{opacity: 1, x: 0, transition:{duration: 2, delay: 1.5}}}
+            exit={{opacity: 0, x: "50%", transition:{duration: .75}}}
+          >
+            <div className='h-fit w-full justify-center align-middle items-center flex flex-col py-3 mb-3 bg-neutral-800 shadow-md shadow-[#7f8285] rounded-xl border-2 border-[#ffffff] transition hover:shadow-lg hover:shadow-pink-300 ease-in-out duration-300 text-white'>
+              <div className='text-7xl animate-text bg-gradient-to-r from-emerald-500 to-pink-400 bg-clip-text text-transparent font-semibold'>11+</div>
+              <div className='text-base '>Profesional Projects</div>
+            </div>
+          </motion.div>
+          <motion.div
+            className='col-span-5'
+            initial={{opacity: 0, x: "50%"}}
+            animate={{opacity: 1, x: 0, transition:{duration: 2, delay: 1.8}}}
+            exit={{opacity: 0, x: "50%", transition:{duration: .75}}}
+          >
+            <div className='h-fit w-full py-3 mb-3 flex flex-col items-center justify-center bg-neutral-800 shadow-md shadow-[#7f8285] rounded-xl border-2 border-[#ffffff] transition hover:shadow-lg hover:shadow-pink-300 ease-in-out duration-300 text-white'>
+              <div className='text-base '>Projects in</div>
+              <div className='text-7xl bg-gradient-to-br from-pink-500 to-pink-300 bg-clip-text text-transparent font-semibold'>8</div>
+              <div className='text-base'>unique industries</div>
+            </div>
+          </motion.div>
+          <motion.div
+            className='col-span-5'
+            initial={{opacity: 0, x: "50%"}}
+            animate={{opacity: 1, x: 0, transition:{duration: 2, delay: 2.1}}}
+            exit={{opacity: 0, x: "50%", transition:{duration: .75}}}
+          >
+            <ResumeButton/>
+          </motion.div>
+        </div>
       </div>
+
+      {/* Mobile 3rd row container*/}
+      <div className='md:hidden grid w-[100%] h-[30%] md:mb-6 mb-3 grid-cols-1 gap-4 md:px-0 px-4 pb-5'>
+        <motion.div
+          className='col-span-1'
+          initial={{opacity: 0, y: "50%"}}
+          animate={{opacity: 1, y: 0, transition:{duration: 2, delay: 1.5}}}
+          exit={{opacity: 0, y: "50%", transition:{duration: 1, delay: .2}}}
+        >
+          <div className='w-full h-fit py-1 flex flex-col items-center justify-center bg-neutral-800 shadow-md shadow-[#7f8285] rounded-xl border-2 border-[#ffffff] transition hover:shadow-lg hover:shadow-pink-300 ease-in-out duration-300'> 
+            <div className='px-3 pt-3 text-lg text-white text-center'>
+              I am dedicated to crafting 
+              <span className='text-lg text-emerald-400'> Responsive</span>, 
+              <span className='text-lg text-pink-400'> Imaginative</span>, and
+              <span className='text-lg text-emerald-400'> Innovative </span>software applications. 
+            </div>
+            <div className='px-3 pt-3 text-lg text-white text-center'>
+              Developing with a <span className='text-lg text-pink-400'>"User-First" </span> approach to create seamless experiences across web and mobile platforms.
+            </div>
+            <div className='px-3 pt-3 text-lg text-white text-center'>
+            I would describe myself as a well-organized and outgoing individual with a <span className='text-lg text-emerald-400'>passion for transforming ideas into reality</span>. Please check out what I can do, and let's create something great together.
+            </div>
+          </div>
+        </motion.div>
+      </div>
+      
       
     </div>
   )
@@ -210,7 +247,7 @@ const ResumeButton = () => {
         className={`
           relative w-full z-0 flex items-center justify-center gap-2 overflow-hidden whitespace-nowrap rounded-lg border-2
           bg-neutral-800 border-white px-4 py-2.5 font-medium
-          text-neutral-100 transition-all duration-300
+          text-neutral-100 md:text-base text-sm transition-all duration-300
           shadow-[0_0px_15px_-1px_rgba(0,0,0,0.1)] hover:shadow-pink-300
             
           before:absolute before:inset-0
