8e1f46265dbc207e7e492819958704cd4cb6683a
nathan-burd <nathanburd.dev@gmail.com>
2024-03-04 12:10:23 -0500

    Added extra responsiveness to project cards for phones with widths below 400px

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      screens: {
+        'xs': '400px'
+      },
       fontFamily: {
         Kaushanscript: ['Kaushan Script']
       },
