b11e08b546420c294cb401819a1aa83dd57148ee
nathan-burd <nathanburd.dev@gmail.com>
2024-03-04 00:23:30 -0500

    Updated fingy bento for mobile responsiveness

--- src/bentoPages/Fingy3DBento.jsx
+++ src/bentoPages/Fingy3DBento.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { motion } from 'framer-motion';
 import ViewSiteButton from '../components/buttons/ViewSiteButton';
 import CloseProjectButton from '../components/buttons/CloseProjectButton';
@@ -11,9 +11,29 @@ import css from "../assets/logos/css.png";
 
 
 function FingyBento({onClose}) {
-  
-  return (
-    <div className="w-full h-full pt-10">
+
+  const [screenWidth, setScreenSize] = useState(window.innerWidth)
+
+  const updateScreenSize = () => {
+    setScreenSize(window.innerWidth)
+  }
+
+  const mobileThreshold = 768;
+  const isMobile = screenWidth < mobileThreshold
+
+  useEffect(() => {
+    // Event listener for screen size changes
+    window.addEventListener('resize', updateScreenSize);
+
+    // Cleanup the event listener when the component unmounts
+    return () => {
+      window.removeEventListener('resize', updateScreenSize);
+    };
+  }, []);
+
+  const WebView = () => {        
+    return (
+      <div className="w-full h-full pt-10">
       {/* Top Row */}
       <div className='w-[100%] h-[15%] mb-6 grid grid-cols-10 gap-6'>
         {/* Title Box */}
@@ -24,8 +44,8 @@ function FingyBento({onClose}) {
           exit={{opacity: 0, y: "-50%", transition:{duration: 1.3, delay: .5}}}
         >
           <div className=' col-span-7 flex flex-col bg-neutral-800 shadow-md shadow-[#7f8285] rounded-xl border-2 border-[#ffffff] transition hover:shadow-lg hover:shadow-pink-300 ease-in-out duration-300'>
-          <div className='text-5xl ml-4 mt-3 pb-2 bg-gradient-to-r from-emerald-400 to-white bg-clip-text text-transparent'>Fingy 3D</div>
-            <div className='text-white text-xl ml-5 mt-1 mb-4'>Web & Mobile Application Development</div>
+          <div className='text-5xl ml-4 mt-3 pb-2 bg-gradient-to-r from-emerald-400 to-white bg-clip-text text-transparent'>Fingy3D</div>
+            <div className='text-white text-xl ml-5 mt-1 mb-4'>Front-End Development</div>
           </div>
         </motion.div>
 
@@ -130,7 +150,149 @@ function FingyBento({onClose}) {
         
       </div>
     </div>
+    );
+  }
+
+  const MobileView = () => {        
+    return (
+      <div className="w-full h-fit">
+        
+        {/* Top Row */}
+        <div className='w-[100%] h-fit mb-3 grid grid-cols-10 gap-3 px-3'>
+          {/* Title Box */}
+          <motion.div
+            style={{ willChange: 'transform' }}
+            className='col-span-7'
+            initial={{opacity: 0, y: "-50%"}}
+            animate={{opacity: 1, y: 0, transition:{duration: 2}}}
+            exit={{opacity: 0, y: "-50%", transition:{duration: 1.3, delay: .5}}}
+          >
+            <div className=' col-span-7 flex flex-col bg-neutral-800 shadow-md shadow-[#7f8285] rounded-xl border-2 border-[#ffffff] transition hover:shadow-lg hover:shadow-pink-300 ease-in-out duration-300'>
+              <div className='text-2xl ml-2 mt-1 bg-gradient-to-r from-emerald-400 to-white bg-clip-text text-transparent'>Fingy3D</div>
+              <div className='text-white text-base ml-2 mt-1 mb-2'>Front-End Development</div>
+            </div>
+          </motion.div>
+
+          {/* Button Box */}
+          <motion.div
+            style={{ willChange: 'transform' }}
+            className='col-span-3'
+            initial={{opacity: 0, y: "-50%"}}
+            animate={{opacity: 1, y: 0, transition:{duration: 2, delay: .2}}}
+            exit={{opacity: 0, y: "-50%", transition:{duration: 1}}}
+          >
+            <div className='h-[100%] col-span-3 flex flex-col justify-center space-y-2'>
+              <ViewSiteButton navLink={'https://fingy3d.com/'}/>
+              <CloseProjectButton onClick={() => {onClose()}}/>
+            </div> 
+          </motion.div>
+        </div>
+
+        {/* Mobile Second Row */}
+        <div className='w-full h-fit mb-3 grid grid-cols-10 gap-3 px-3'>
+          {/* Left Box */}
+          <motion.div
+            style={{ willChange: 'transform' }}
+            className='col-span-10'
+            initial={{opacity: 0, x: "50%"}}
+            animate={{opacity: 1, x: 0, transition:{duration: 2, delay: .5}}}
+            exit={{opacity: 0, x: "50%", transition:{duration: 1, delay: .2}}}
+          >
+            <div className='h-fit col-span-10 align-middle items-center flex flex-col pb-2 bg-neutral-800 shadow-md shadow-[#7f8285] rounded-xl border-2 border-[#ffffff] transition hover:shadow-lg hover:shadow-pink-300 ease-in-out duration-300 text-white'>
+              <div className='w-fit mt-2 text-xl font-semibold bg-gradient-to-r from-emerald-600 to-emerald-300 bg-clip-text text-transparent'>
+                Project Description
+              </div>
+              <div className='text-base mt-1 mx-2 text-center'>
+                An award winning revolutionary application aiming to make finger prosthetics accessible to anyone. Utilizing a phone's camera, the user snaps a few guided pictures and the FingyEngine AI scans and sizes their custom prosthetic for an 'only you' fit.</div>
+              </div>
+          </motion.div>
+        </div>
+
+        {/* Mobile third,fourth, fifth Row */}
+        <div className='w-full h-fit mb-3 grid grid-cols-10 gap-3 px-3 pb-16'>
+          {/* Tech Stack Box*/}
+          <motion.div
+            style={{ willChange: 'transform' }}
+            className='col-span-10'
+            initial={{opacity: 0, x: "50%"}}
+            animate={{opacity: 1, x: 0, transition:{duration: 2, delay: 1}}}
+            exit={{opacity: 0, x: "50%", transition:{duration: 1, delay: .2}}}
+          >
+            <div className='h-fit col-span-10 pb-4 bg-neutral-800 shadow-md shadow-[#7f8285] rounded-xl border-2 border-[#ffffff] transition hover:shadow-lg hover:shadow-pink-300 ease-in-out duration-300'> 
+              <div className='ml-1 mt-1 text-lg text-white text-center'>
+                {'</>'} Tech Stack
+              </div>
+              <div className="w-full grid grid-cols-4 text-center text-white font-medium text-xs">
+                <SkillBox imageImport={react} skill="React"/>
+                <SkillBox imageImport={react} skill="React Native"/>
+                <SkillBox imageImport={Sass} skill="Sass"/>
+                <SkillBox imageImport={css} skill="CSS"/>
+              </div>
+            </div>
+          </motion.div>
+
+          {/* Numbered Box */}
+          <motion.div
+            style={{ willChange: 'transform' }}
+            className='col-span-10'
+            initial={{opacity: 0, x: "50%"}}
+            animate={{opacity: 1, x: 0, transition:{duration: 2, delay: 1.5}}}
+            exit={{opacity: 0, x: "50%", transition:{duration: 1, delay: .2}}}
+          >
+            <div className='h-fit justify-center align-middle items-center flex flex-col pb-2 bg-neutral-800 shadow-md shadow-[#7f8285] rounded-xl border-2 border-[#ffffff] transition hover:shadow-lg hover:shadow-pink-300 ease-in-out duration-300 text-white'>
+              <div className='text-base mt-2'>Decreased Load times by</div>
+              <div className='text-6xl my-2 animate-text bg-gradient-to-r from-emerald-500 to-pink-400 bg-clip-text text-transparent font-semibold'>~30%</div>
+              <div className='text-base text-center'>enhancing front-end performance</div>
+            </div>
+          </motion.div>
+
+          {/* Contributions Box*/}
+          <motion.div
+            style={{ willChange: 'transform' }}
+            className='col-span-10'
+            initial={{opacity: 0, x: "50%"}}
+            animate={{opacity: 1, x: 0, transition:{duration: 2, delay: 2}}}
+            exit={{opacity: 0, x: "50%", transition:{duration: 1, delay: .2}}}
+          >
+            <div className='h-fit pr-2 pb-2 flex flex-col bg-neutral-800 shadow-md shadow-[#7f8285] rounded-xl border-2 border-[#ffffff] transition hover:shadow-lg hover:shadow-pink-300 ease-in-out duration-300 '>  
+              <div className='w-fit m-2 text-lg font-medium bg-gradient-to-r from-pink-500 to-pink-300 bg-clip-text text-transparent'>
+                What impact did I have?
+              </div>
+              <ImpactText>
+                Designed and <span className='text-lg text-emerald-400'>implemented a custom camera page</span>, enhancing the user experience by enabling precise finger image capture for prosthetic fitting  
+              </ImpactText>
+              <ImpactText>
+                <span className='text-lg text-emerald-400'>Crafted a streamlined submission flow</span>, ensuring efficient data collection and processing to facilitate the prosthetic fitting process
+              </ImpactText>
+              <ImpactText>
+                Increased successful user submissions by <span className='text-lg text-emerald-400'>over 200%</span> by implementing various bug fixes and improvements
+              </ImpactText>  
+              <ImpactText>
+                Prioritized <span className='text-lg text-emerald-400'>user-centered design principles</span> to ensure the application's ease of use and accessibility
+              </ImpactText>
+            </div>
+          </motion.div>
+
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    !isMobile ? (
+      <WebView/>
+    ) : (
+      <MobileView/>
+    )
   )
 }
 
 export default FingyBento;
+
+const ImpactText = (props) => {
+  return (
+    <div className='text-white text-base mb-2 ml-2'>
+      {props.children}
+    </div>
+  )
+}
