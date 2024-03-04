ed9f3d43a0cb8dafec93d7d42cd81e1f178c6225
nathan-burd <nathanburd.dev@gmail.com>
2024-03-04 00:23:37 -0500

    Updated gigup bento for mobile responsiveness

--- src/bentoPages/GigUpBento.jsx
+++ src/bentoPages/GigUpBento.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { motion } from 'framer-motion';
 import ViewSiteButton from '../components/buttons/ViewSiteButton';
 import CloseProjectButton from '../components/buttons/CloseProjectButton';
@@ -13,9 +13,29 @@ import BigQuery from "../assets/logos/BigQuery.png"
 import eas from "../assets/logos/eas.png"
 
 export default function GigUpBento({onClose}) {
-  
-  return (
-    <div className="w-full h-full pt-10">
+
+  const [screenWidth, setScreenSize] = useState(window.innerWidth)
+
+  const updateScreenSize = () => {
+    setScreenSize(window.innerWidth)
+  }
+
+  const mobileThreshold = 768;
+  const isMobile = screenWidth < mobileThreshold
+
+  useEffect(() => {
+    // Event listener for screen size changes
+    window.addEventListener('resize', updateScreenSize);
+
+    // Cleanup the event listener when the component unmounts
+    return () => {
+      window.removeEventListener('resize', updateScreenSize);
+    };
+  }, []);
+
+  const WebView = () => {        
+    return (
+      <div className="w-full h-full pt-10">
       {/* Top Row */}
       <div className='w-[100%] h-[15%] mb-6 grid grid-cols-10 gap-6'>
         {/* Title Box */}
@@ -27,7 +47,7 @@ export default function GigUpBento({onClose}) {
         >
           <div className=' col-span-7 flex flex-col bg-neutral-800 shadow-md shadow-[#7f8285] rounded-xl border-2 border-[#ffffff] transition hover:shadow-lg hover:shadow-pink-300 ease-in-out duration-300'>
             <div className='text-5xl ml-4 mt-3 pb-2 bg-gradient-to-r from-emerald-400 to-white bg-clip-text text-transparent'>GigUp</div>
-            <div className='text-white text-xl ml-5 mt-1 mb-4'>Web & Mobile Application Development</div>
+            <div className='text-white text-xl ml-5 mt-1 mb-4'>Full Stack Development</div>
           </div>
         </motion.div>
 
@@ -137,6 +157,154 @@ export default function GigUpBento({onClose}) {
         </motion.div>
         
       </div>
+    </div>
+    );
+  }
+
+  const MobileView = () => {        
+    return (
+      <div className="w-full h-fit">
+        
+        {/* Top Row */}
+        <div className='w-[100%] h-fit mb-3 grid grid-cols-10 gap-3 px-3'>
+          {/* Title Box */}
+          <motion.div
+            style={{ willChange: 'transform' }}
+            className='col-span-7'
+            initial={{opacity: 0, y: "-50%"}}
+            animate={{opacity: 1, y: 0, transition:{duration: 2}}}
+            exit={{opacity: 0, y: "-50%", transition:{duration: 1.3, delay: .5}}}
+          >
+            <div className=' col-span-7 flex flex-col bg-neutral-800 shadow-md shadow-[#7f8285] rounded-xl border-2 border-[#ffffff] transition hover:shadow-lg hover:shadow-pink-300 ease-in-out duration-300'>
+              <div className='text-2xl ml-2 mt-1 bg-gradient-to-r from-emerald-400 to-white bg-clip-text text-transparent'>GigUp</div>
+              <div className='text-white text-base ml-2 mt-1 mb-2'>Full Stack Development</div>
+            </div>
+          </motion.div>
+
+          {/* Button Box */}
+          <motion.div
+            style={{ willChange: 'transform' }}
+            className='col-span-3'
+            initial={{opacity: 0, y: "-50%"}}
+            animate={{opacity: 1, y: 0, transition:{duration: 2, delay: .2}}}
+            exit={{opacity: 0, y: "-50%", transition:{duration: 1}}}
+          >
+            <div className='h-[100%] col-span-3 flex flex-col justify-center space-y-2'>
+            <ViewSiteButton navLink={'https://www.gigup.me/'}/>
+              <CloseProjectButton onClick={() => {onClose()}}/>
+            </div> 
+          </motion.div>
+        </div>
+
+        {/* Mobile Second Row */}
+        <div className='w-full h-fit mb-3 grid grid-cols-10 gap-3 px-3'>
+          {/* Left Box */}
+          <motion.div
+            style={{ willChange: 'transform' }}
+            className='col-span-10'
+            initial={{opacity: 0, x: "50%"}}
+            animate={{opacity: 1, x: 0, transition:{duration: 2, delay: .5}}}
+            exit={{opacity: 0, x: "50%", transition:{duration: 1, delay: .2}}}
+          >
+            <div className='h-fit col-span-10 align-middle items-center flex flex-col pb-2 bg-neutral-800 shadow-md shadow-[#7f8285] rounded-xl border-2 border-[#ffffff] transition hover:shadow-lg hover:shadow-pink-300 ease-in-out duration-300 text-white'>
+              <div className='w-fit mt-2 text-xl font-semibold bg-gradient-to-r from-emerald-600 to-emerald-300 bg-clip-text text-transparent'>
+                Project Description
+              </div>
+              <div className='text-base mt-1 mx-2 text-center'>
+                A job finding platform designed to empower candidates with its user centric design, putting the control in their hands. GigUp matches potential candidates with part-time positions based on attributes such as availability, distance, and experience.  
+              </div>
+            </div>
+          </motion.div>
+        </div>
+
+        {/* Mobile third,fourth, fifth Row */}
+        <div className='w-full h-fit mb-3 grid grid-cols-10 gap-3 px-3 pb-16'>
+          {/* Tech Stack Box*/}
+          <motion.div
+            style={{ willChange: 'transform' }}
+            className='col-span-10'
+            initial={{opacity: 0, x: "50%"}}
+            animate={{opacity: 1, x: 0, transition:{duration: 2, delay: 1}}}
+            exit={{opacity: 0, x: "50%", transition:{duration: 1, delay: .2}}}
+          >
+            <div className='h-fit col-span-10 pb-4 bg-neutral-800 shadow-md shadow-[#7f8285] rounded-xl border-2 border-[#ffffff] transition hover:shadow-lg hover:shadow-pink-300 ease-in-out duration-300'> 
+              <div className='ml-1 mt-1 text-lg text-white text-center'>
+                {'</>'} Tech Stack
+              </div>
+              <div className="w-full grid grid-cols-6 text-center text-white font-medium text-xs">
+                <SkillBox imageImport={react} skill="React"/>
+                <SkillBox imageImport={react} skill="React Native"/>
+                <SkillBox imageImport={firebase} skill="Firebase"/>
+                <SkillBox imageImport={eas} skill="EAS"/>
+                <SkillBox imageImport={BigQuery} skill="BigQuery"/>
+                <SkillBox imageImport={sql} skill="SQL"/> 
+              </div>
+            </div>
+          </motion.div>
+
+          {/* Numbered Box */}
+          <motion.div
+            style={{ willChange: 'transform' }}
+            className='col-span-10'
+            initial={{opacity: 0, x: "50%"}}
+            animate={{opacity: 1, x: 0, transition:{duration: 2, delay: 1.5}}}
+            exit={{opacity: 0, x: "50%", transition:{duration: 1, delay: .2}}}
+          >
+            <div className='h-fit justify-center align-middle items-center flex flex-col pb-2 bg-neutral-800 shadow-md shadow-[#7f8285] rounded-xl border-2 border-[#ffffff] transition hover:shadow-lg hover:shadow-pink-300 ease-in-out duration-300 text-white'>
+              <div className='text-base mt-2'>Peaked at</div>
+              <div className='text-6xl my-2 animate-text bg-gradient-to-r from-emerald-500 to-pink-400 bg-clip-text text-transparent font-semibold'>{`> 250`}</div>
+              <div className='text-base text-center'>active users</div>
+            </div>
+          </motion.div>
+
+          {/* Contributions Box*/}
+          <motion.div
+            style={{ willChange: 'transform' }}
+            className='col-span-10'
+            initial={{opacity: 0, x: "50%"}}
+            animate={{opacity: 1, x: 0, transition:{duration: 2, delay: 2}}}
+            exit={{opacity: 0, x: "50%", transition:{duration: 1, delay: .2}}}
+          >
+            <div className='h-fit pr-2 pb-2 flex flex-col bg-neutral-800 shadow-md shadow-[#7f8285] rounded-xl border-2 border-[#ffffff] transition hover:shadow-lg hover:shadow-pink-300 ease-in-out duration-300 '>  
+              <div className='w-fit m-2 text-lg font-medium bg-gradient-to-r from-pink-500 to-pink-300 bg-clip-text text-transparent'>
+                What impact did I have?
+              </div>
+              <ImpactText>
+                Assumed <span className='text-lg text-emerald-400'>full-stack development</span> responsibilities, from conceptualization to deployment
+              </ImpactText>
+              <ImpactText>
+                Played an instrumental role in <span className='text-lg text-emerald-400'>feature ideation, design, and implementation</span> 
+              </ImpactText>
+              <ImpactText>
+                Developed <span className='text-lg text-emerald-400'>reusable frontend components</span> prioritizing responsive design across various devices
+              </ImpactText>  
+              <ImpactText>
+                Heavily influenced the overall <span className='text-lg text-emerald-400'>user interface</span> and <span className='text-lg text-emerald-400'>user experience</span> of the mobile and web applications  
+              </ImpactText>
+              <ImpactText>
+                Created <span className='text-lg text-emerald-400'>automated daily/weekly reporting</span> by integrating BigQuery and Data Studio with the Firebase backend
+              </ImpactText>
+            </div>
+          </motion.div>
+
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    !isMobile ? (
+      <WebView/>
+    ) : (
+      <MobileView/>
+    )
+  )
+}
+
+const ImpactText = (props) => {
+  return (
+    <div className='text-white text-base mb-2 ml-2'>
+      {props.children}
     </div>
   )
 }
